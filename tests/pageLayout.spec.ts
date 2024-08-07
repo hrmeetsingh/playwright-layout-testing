83abcb6f30bd8d078dedb7ec4168da765cad1d7f
Harmeet Singh <er.harmeetsingh@hotmail.com>
2024-08-06 19:17:37 -0600

    Hooks for page setup

--- tests/pageLayout.spec.ts
+++ tests/pageLayout.spec.ts
@@ -1,11 +1,16 @@
+import { Page } from "@playwright/test";
 import { test, expect } from "../fixtures/layout-matchers";
 import { PlaywrightDevPage } from "../pages/playwright-dev-page";
 
-test("Page layout test - element left of other element", async ({ page }) => {
-  const playwrightDev = new PlaywrightDevPage(page);
+let playwrightDev: PlaywrightDevPage;
+
+test.beforeEach(async ({ page }) => {
+  playwrightDev = new PlaywrightDevPage(page);
   await playwrightDev.goto();
   await playwrightDev.getStarted();
+});
 
+test("Page layout test - element left of other element", async ({ page }) => {
   // nav link is horizontally left of the page header
   await expect(playwrightDev.writingTestsNavLink).toBeLeftOf(
     playwrightDev.installationPageHeader,
@@ -13,10 +18,6 @@ test("Page layout test - element left of other element", async ({ page }) => {
 });
 
 test("Page layout test - element above other element", async ({ page }) => {
-  const playwrightDev = new PlaywrightDevPage(page);
-  await playwrightDev.goto();
-  await playwrightDev.getStarted();
-
   // nav links are vertically positioned
   await expect(playwrightDev.writingTestsNavLink).toBeAbove(
     playwrightDev.supportedLanguagesNavLink,
@@ -26,10 +27,6 @@ test("Page layout test - element above other element", async ({ page }) => {
 test("Page layout test - element above other element and left vertical aligned", async ({
   page,
 }) => {
-  const playwrightDev = new PlaywrightDevPage(page);
-  await playwrightDev.goto();
-  await playwrightDev.getStarted();
-
   // nav links are vertically positioned and left edges vertically aligned
   await expect(playwrightDev.installationNavSubLink).toBeAbove(
     playwrightDev.writingTestsNavLink,
@@ -43,10 +40,6 @@ test("Page layout test - element above other element and left vertical aligned",
 test("Page layout test - element left of other element and horizontally aligned", async ({
   page,
 }) => {
-  const playwrightDev = new PlaywrightDevPage(page);
-  await playwrightDev.goto();
-  await playwrightDev.getStarted();
-
   // nav links are horizontally positioned and top edges horizontally aligned
   await expect(playwrightDev.breadcrumbGettingStartedLink).toBeLeftOf(
     playwrightDev.breadcrumbInstallationLink,
